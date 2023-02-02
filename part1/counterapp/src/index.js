3b7da9480ba87efde91aa547852375cd86052dee
Amutha Muhunthan <tamamutha@gmail.com>
2023-02-02 16:49:59 +1100

    add counter app

--- part1/counterapp/src/index.js
+++ part1/counterapp/src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import App from './App'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import './App.css'
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<App />)
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h4>Counter App</h4>
+      <div id='count'>{store.getState()}</div>
+      <button id='add' onClick={(e) => store.dispatch({ type: 'INCREMENT' })}>
+        plus
+      </button>
+      <button id='minus' onClick={(e) => store.dispatch({ type: 'DECREMENT' })}>
+        minus
+      </button>
+      <button id='reset' onClick={(e) => store.dispatch({ type: 'ZERO' })}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
